18701bc03718fa643c2972422f003ba357df2279
taketo <taketowinter@gmail.com>
2022-11-17 11:17:43 +0000

    pタグを追加しました

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ document.getElementById("app").innerHTML = `
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here our world</a>.
 </div>
 `;
